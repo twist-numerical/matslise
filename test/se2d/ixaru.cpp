c35d4013ddb66a417a4057ff41af85716b2f6db0
Toon Baeyens <toon.baeyens@ugent.be>
2019-12-03 09:25:17 +0100

    test high n

--- test/se2d/ixaru.cpp
+++ test/se2d/ixaru.cpp
@@ -107,3 +107,52 @@ TEST_CASE("Eigenfunctions ixaru auto", "[se2d][eigenfunctions][ixaru][auto]") {
     }
     checkOrthonormality(p2, eigenvalues_simple.begin(), eigenvalues_simple.end());
 }
+
+TEST_CASE("Eigenfunctions ixaru auto high n", "[se2d][eigenfunctions][ixaru][auto][slow]") {
+    SE2D<> p2(
+            [](double x, double y) -> double {
+                return (1 + x * x) * (1 + y * y);
+            },
+            {{-5.5, 5.5}, -5.5, 5.5},
+            Options2<>().tolerance(1e-5).stepsPerSector(1).N(20).nested(Options1<>().tolerance(1e-7)));
+    pair<double, int> eigenvalues[] = {
+            {3.1959181,  1},
+            {5.5267439,  2},
+            {7.5578033,  1},
+            {8.0312723,  1},
+            {8.4445814,  1},
+            {9.9280611,  2},
+            {11.3118171, 2},
+            {12.1032536, 1},
+            {12.2011790, 1},
+            {13.3323313, 1}
+    };
+
+
+    ArrayXd x(3);
+    x << -1, 0, 1;
+    double E;
+    int multiplicity;
+    for (int i = 0; i < 9; ++i) {
+        double error = get<0>(p2.calculateError((get<0>(eigenvalues[i]) + get<0>(eigenvalues[i + 1])) / 2));
+        CHECK(abs(error) > 1e-3);
+    }
+    vector<double> eigenvalues_simple;
+    for (auto &Emult  : eigenvalues) {
+        tie(E, multiplicity) = Emult;
+        eigenvalues_simple.push_back(E);
+        const pair<double, double> &error = p2.calculateError(E);
+        CHECK(abs(error.first) < 1e-3);
+
+        if (E < 6) {
+            double El = p2.findEigenvalue(E - 0.01);
+            double Em = p2.findEigenvalue(E + 0.01);
+            CHECK(Approx(El).margin(1e-7) == E);
+            CHECK(Approx(Em).margin(1e-7) == E);
+        }
+
+        const vector<Array<double, -1, -1>> f = p2.computeEigenfunction(E, x, x);
+        CHECK(f.size() == multiplicity);
+    }
+    checkOrthonormality(p2, eigenvalues_simple.begin(), eigenvalues_simple.end());
+}
