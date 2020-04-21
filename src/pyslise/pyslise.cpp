a03c937e5982ad4f81ab9f690876c31ab369672a
Toon Baeyens <toon.baeyens@gmail.com>
2020-04-21 13:22:58 +0200

    pyslise abstratmatslise

--- src/pyslise/pyslise.cpp
+++ src/pyslise/pyslise.cpp
@@ -4,8 +4,102 @@
 #include "module.h"
 
 void pySlise(py::module &m) {
+    py::class_<AbstractMatslise<double>>(m, "AbstractPySlise")
+            .def("eigenvalues",
+                 [](AbstractMatslise<double> &m, double Emin, double Emax, const Vector2d &left,
+                    const optional<Vector2d> &_right)
+                         -> vector<pair<int, double>> {
+                     const Vector2d &right = _right ? *_right : left;
+                     return m.eigenvalues(Emin, Emax, make_y(left), make_y(right));
+                 }, R""""(\
+Calculate the eigenvalues in an interval [Emin; Emax]. The boundary conditions have to be specified.
 
-    py::class_<Matslise<>>(m, "PySlise")
+:param float Emin, Emax: the ends of the search interval.
+:param (float,float) left, right: the boundary conditions. The corresponding eigenfunctions will have as left value a scaling of left[0] and derivative left[1].
+
+:returns: a list of tuples. Each tuples contains the index and the eigenvalue with that index.
+)"""",
+                 py::arg("Emin"), py::arg("Emax"), py::arg("left"), py::arg("right") = optional<Vector2d>())
+            .def("eigenvaluesByIndex",
+                 [](AbstractMatslise<double> &m, int Imin, int Imax, const Vector2d &left,
+                    const optional<Vector2d> &_right) -> vector<pair<int, double>> {
+                     const Vector2d &right = _right ? *_right : left;
+                     return m.eigenvaluesByIndex(Imin, Imax, make_y(left), make_y(right));
+                 }, R""""(\
+Calculate all eigenvalues with index between Imin and Imax. The first eigenvalue has index 0. Imin inclusive, Imax exclusive.
+
+:param int Imin: the first eigenvalue to find, by index.
+:param int Imax: only the first Imax eigenvalues will be considered.
+:param (float,float) left, right: the boundary conditions.
+
+:returns: a list of tuples. Each tuples contains the index and the eigenvalue with that index.
+)"""",
+                 py::arg("Imin"), py::arg("Imax"), py::arg("left"), py::arg("right") = optional<Vector2d>())
+            .def("eigenvalueError",
+                 [](AbstractMatslise<double> &m, double E, const Vector2d &left, const optional<Vector2d> &_right,
+                    int index)
+                         -> double {
+                     const Vector2d &right = _right ? *_right : left;
+                     return m.eigenvalueError(E, make_y(left), make_y(right), index);
+                 }, R""""(\
+Calculate the error for a given eigenvalue. It will use a less accurate method to estimate another (worse) guess for that eigenvalue. The true error on the given eigenvalue will be less than the value returned by this method.
+
+:param float E: the eigenvalue to calculate the error for.
+:param (float,float) left, right: the boundary conditions.
+
+:returns: the error.
+)"""",
+                 py::arg("E"), py::arg("left"), py::arg("right") = optional<Vector2d>(), py::arg("index") = -1)
+            .def("eigenfunction",
+                 [](AbstractMatslise<double> &m, double E, const ArrayXd &xs, const Vector2d &left,
+                    const optional<Vector2d> &_right, int index)
+                         -> tuple<ArrayXd, ArrayXd> {
+                     const Vector2d &right = _right ? *_right : left;
+                     auto ysY = m.eigenfunction(E, make_y(left), make_y(right), xs, index);
+                     ArrayXd ys(ysY.size());
+                     ArrayXd dys(ysY.size());
+                     for (Eigen::Index i = 0; i < ysY.size(); ++i) {
+                         ys[i] = ysY[i].y[0];
+                         dys[i] = ysY[i].y[1];
+                     }
+                     return make_tuple(ys, dys);
+                 }, R""""(\
+Calculate the eigenfunction corresponding to the eigenvalue E in the points xs.
+
+:param float E: the eigenvalue.
+:param (float,float) left, right: the boundary conditions.
+:param xs: the points to calculate the eigenfunction for.
+
+:returns: a pair of lists which each a length of len(xs). The first list contains the values of the eigenfunction in the points xs. The second contains the derivative of the eigenfunction in those points.
+)"""",
+                 py::arg("E"), py::arg("xs"), py::arg("left"), py::arg("right") = optional<Vector2d>(),
+                 py::arg("index") = -1)
+            .def("eigenfunction",
+                 [](AbstractMatslise<double> &m, double E, const Vector2d &left,
+                    const optional<Vector2d> &_right, int index) -> function<pair<double, double>(double)> {
+                     const Vector2d &right = _right ? *_right : left;
+                     function<Y<>(double)> calculator = m.eigenfunctionCalculator(E, make_y(left), make_y(right),
+                                                                                  index);
+                     return [calculator](double x) -> pair<double, double> {
+                         Y<> y = calculator(x);
+                         return make_pair(y.y[0], y.y[1]);
+                     };
+                 }, R""""(\
+Returns the eigenfunction corresponding to the eigenvalue E as a python function. The returned function can be evaluated in all the points in the domain.
+
+:param float E: the eigenvalue.
+:param (float,float) left, [right]: the boundary conditions.
+:param int [index]: the index of the eigenvalue
+
+:returns: a function that takes a value and returns a tuple with the eigenfunction and derivative in that value.
+)"""",
+                 py::arg("E"), py::arg("left"), py::arg("right") = optional<Vector2d>(), py::arg("index") = -1)
+            .def_property_readonly("domain", [](const Matslise<> &matslise) {
+                return std::pair<double, double>(matslise.domain.min, matslise.domain.max);
+            });
+
+
+    py::class_<Matslise<>, AbstractMatslise<double>>(m, "PySlise")
             .def(py::init([](const function<double(double)> &V, double min, double max, double tolerance) {
                 return new Matslise<>(V, min, max, tolerance);
             }), R""""(\
@@ -54,84 +148,6 @@ For a given E and initial condition in point a, propagate the solution of the Sc
 
 :returns: a tuple of length three. The first element is a tuple with the value and the derivative in the point b. The second element contains the derivative to E of the first element. The third element is the angle found by the Prüfer transformation.)"""",
                  py::arg("E"), py::arg("y"), py::arg("dy"), py::arg("a"), py::arg("b"))
-            .def("eigenvalues",
-                 [](Matslise<> &m, double Emin, double Emax, const Vector2d &left, const Vector2d &right)
-                         -> vector<pair<int, double>> {
-                     return m.eigenvalues(Emin, Emax, make_y(left), make_y(right));
-                 }, R""""(\
-Calculate the eigenvalues in an interval [Emin; Emax]. The boundary conditions have to be specified.
-
-:param float Emin, Emax: the ends of the search interval.
-:param (float,float) left, right: the boundary conditions. The corresponding eigenfunctions will have as left value a scaling of left[0] and derivative left[1].
-
-:returns: a list of tuples. Each tuples contains the index and the eigenvalue with that index.
-)"""",
-                 py::arg("Emin"), py::arg("Emax"), py::arg("left"), py::arg("right"))
-            .def("eigenvaluesByIndex",
-                 [](Matslise<> &m, int Imin, int Imax, const Vector2d &left, const Vector2d &right)
-                         -> vector<pair<int, double>> {
-                     return m.eigenvaluesByIndex(Imin, Imax, make_y(left), make_y(right));
-                 }, R""""(\
-Calculate all eigenvalues with index between Imin and Imax. The first eigenvalue has index 0. Imin inclusive, Imax exclusive.
-
-:param int Imin: the first eigenvalue to find, by index.
-:param int Imax: only the first Imax eigenvalues will be considered.
-:param (float,float) left, right: the boundary conditions.
-
-:returns: a list of tuples. Each tuples contains the index and the eigenvalue with that index.
-)"""",
-                 py::arg("Imin"), py::arg("Imax"), py::arg("left"), py::arg("right"))
-            .def("eigenvalueError",
-                 [](Matslise<> &m, double E, const Vector2d &left, const Vector2d &right)
-                         -> double {
-                     return m.eigenvalueError(E, make_y(left), make_y(right));
-                 }, R""""(\
-Calculate the error for a given eigenvalue. It will use a less accurate method to estimate another (worse) guess for that eigenvalue. The true error on the given eigenvalue will be less than the value returned by this method.
-
-:param float E: the eigenvalue to calculate the error for.
-:param (float,float) left, right: the boundary conditions.
-
-:returns: the error.
-)"""",
-                 py::arg("E"), py::arg("left"), py::arg("right"))
-            .def("eigenfunction",
-                 [](Matslise<> &m, double E, const Vector2d &left, const Vector2d &right, const ArrayXd &xs)
-                         -> tuple<ArrayXd, ArrayXd> {
-                     auto ysY = m.eigenfunction(E, make_y(left), make_y(right), xs);
-                     ArrayXd ys(ysY.size());
-                     ArrayXd dys(ysY.size());
-                     for (Eigen::Index i = 0; i < ysY.size(); ++i) {
-                         ys[i] = ysY[i].y[0];
-                         dys[i] = ysY[i].y[1];
-                     }
-                     return make_tuple(ys, dys);
-                 }, R""""(\
-Calculate the eigenfunction corresponding to the eigenvalue E in the points xs.
-
-:param float E: the eigenvalue.
-:param (float,float) left, right: the boundary conditions.
-:param xs: the points to calculate the eigenfunction for.
-
-:returns: a pair of lists which each a length of len(xs). The first list contains the values of the eigenfunction in the points xs. The second contains the derivative of the eigenfunction in those points.
-)"""",
-                 py::arg("E"), py::arg("left"), py::arg("right"), py::arg("xs"))
-            .def("eigenfunction",
-                 [](Matslise<> &m, double E, const Vector2d &left,
-                    const Vector2d &right) -> function<pair<double, double>(double)> {
-                     function<Y<>(double)> calculator = m.eigenfunctionCalculator(E, make_y(left), make_y(right));
-                     return [calculator](double x) -> pair<double, double> {
-                         Y<> y = calculator(x);
-                         return make_pair(y.y[0], y.y[1]);
-                     };
-                 }, R""""(\
-Returns the eigenfunction corresponding to the eigenvalue E as a python function. The returned function can be evaluated in all the points in the domain.
-
-:param float E: the eigenvalue.
-:param (float,float) left, right: the boundary conditions.
-
-:returns: a function that takes a value and returns a tuple with the eigenfunction and derivative in that value.
-)"""",
-                 py::arg("E"), py::arg("left"), py::arg("right"))
             .def("__error",
                  [](Matslise<> &m, double E, const Vector2d &left, const Vector2d &right)
                          -> tuple<double, double, double> {
@@ -140,13 +156,11 @@ Returns the eigenfunction corresponding to the eigenvalue E as a python function
                  py::arg("E"), py::arg("left"), py::arg("right"))
             .def_readonly("__sectorCount", &Matslise<>::sectorCount)
             .def_readonly("__matchIndex", &Matslise<>::matchIndex)
-            .def_readonly("__min", &Matslise<>::xmin)
-            .def_readonly("__max", &Matslise<>::xmax)
             .def("__sector", [](Matslise<> &p, int i) -> Matslise<>::Sector * {
                 return p.sectors[i];
             }, py::return_value_policy::reference);
 
-    py::class_<MatsliseHalf<>>(m, "PySliseHalf")
+    py::class_<MatsliseHalf<>, AbstractMatslise<double>>(m, "PySliseHalf")
             .def(py::init([](const function<double(double)> &V, double xmax, double tolerance) {
                 return new MatsliseHalf<>(V, xmax, tolerance);
             }), R""""(\
@@ -158,82 +172,7 @@ Note: only one of steps and tolerance have to be set.
 :param float min, max: the ends of the domain.
 :param int steps: the number of steps to take.
 :param int tolerance: automatically choose steps with at least the given accuracy.
-)"""", py::arg("V"), py::arg("xmax"), py::arg("tolerance") = 1e-8)
-            .def("eigenvalues", [](MatsliseHalf<double> &m, double Emin, double Emax,
-                                   const Vector2d &side) -> vector<pair<int, double>> {
-                return m.eigenvalues(Emin, Emax, make_y(side));
-            }, R""""(\
-Calculate the eigenvalues in an interval [Emin; Emax]. The boundary conditions have to be specified.
-
-:param float Emin, Emax: the ends of the search interval.
-:param (float,float) side: the symmetric boundary conditions.
-
-:returns: a list of tuples. Each tuples contains the index and the eigenvalue with that index.
-)"""", py::arg("Emin"), py::arg("Emax"), py::arg("side"))
-            .def("eigenvaluesByIndex",
-                 [](MatsliseHalf<> &m, int Imin, int Imax, const Vector2d &side) -> vector<pair<int, double>> {
-                     return m.eigenvaluesByIndex(Imin, Imax, make_y(side));
-                 }, R""""(\
-Calculate all eigenvalues with index between Imin and Imax. The first eigenvalue has index 0. Imin inclusive, Imax exclusive.
-
-:param int Imin: the first eigenvalue to find, by index.
-:param int Imax: only the first Imax eigenvalues will be considered.
-:param (float,float) side: the symmetric boundary conditions.
-
-:returns: a list of tuples. Each tuples contains the index and the eigenvalue with that index.
-)"""",
-                 py::arg("Imin"), py::arg("Imax"), py::arg("side"))
-            .def("eigenvalueError",
-                 [](MatsliseHalf<> &m, double E, const Vector2d &side, int even) -> double {
-                     return m.eigenvalueError(E, make_y(side), even);
-                 }, R""""(\
-Calculate the error for a given eigenvalue. It will use a less accurate method to estimate another (worse) guess for that eigenvalue. The true error on the given eigenvalue will be less than the value returned by this method.
-
-:param float E: the eigenvalue to calculate the error for.
-:param (float,float) side: the symmetric boundary conditions.
-:param int even: indication if the eigenvalue is even (1) or odd (0). Defaults to auto (-1).
-
-:returns: the error.
-)"""",
-                 py::arg("E"), py::arg("side"), py::arg("even") = -1)
-            .def("eigenfunction",
-                 [](MatsliseHalf<> &m, double E, const Vector2d &side, const ArrayXd &xs, int even)
-                         -> tuple<ArrayXd, ArrayXd> {
-                     auto ysY = m.eigenfunction(E, make_y(side), xs, even);
-                     ArrayXd ys(ysY.size());
-                     ArrayXd dys(ysY.size());
-                     for (Eigen::Index i = 0; i < ysY.size(); ++i) {
-                         ys[i] = ysY[i].y[0];
-                         dys[i] = ysY[i].y[1];
-                     }
-                     return make_tuple(ys, dys);
-                 }, R""""(\
-Calculate the eigenfunction corresponding to the eigenvalue E in the points xs.
-
-:param float E: the eigenvalue.
-:param (float,float) side: the symmetric boundary conditions.
-:param xs: the points to calculate the eigenfunction for.
-:param int even: indication if the eigenvalue is even (1) or odd (0). Defaults to auto (-1).
-
-:returns: a pair of lists which each a length of len(xs). The first list contains the values of the eigenfunction in the points xs. The second contains the derivative of the eigenfunction in those points.
-)"""", py::arg("E"), py::arg("side"), py::arg("xs"), py::arg("even") = -1)
-            .def("eigenfunction",
-                 [](MatsliseHalf<> &m, double E, const Vector2d &side, int even) -> function<pair<double, double>(
-                         double)> {
-                     function<Y<>(double)> calculator = m.eigenfunctionCalculator(E, make_y(side), even);
-                     return [calculator](double x) -> pair<double, double> {
-                         Y<> y = calculator(x);
-                         return make_pair(y.y[0], y.y[1]);
-                     };
-                 }, R""""(\
-Returns the eigenfunction corresponding to the eigenvalue E as a python function. The returned function can be evaluated in all the points in the domain.
-
-:param float E: the eigenvalue.
-:param (float,float) side: the left and right boundary condition.
-
-:returns: a function that takes a value and returns a tuple with the eigenfunction and derivative in that value.
-)"""",
-                 py::arg("E"), py::arg("side"), py::arg("even") = -1);
+)"""", py::arg("V"), py::arg("xmax"), py::arg("tolerance") = 1e-8);
 
     py::class_<Matslise<>::Sector>(m, "PySliseSector")
             .def_readonly("min", &Matslise<>::Sector::min)
