290340917f13f626f67db1f115ee7ad2d494b6d8
Toon Baeyens <toon.baeyens@gmail.com>
2025-10-26 20:33:26 +0100

    Update python versions

--- docker_build.sh
+++ docker_build.sh
@@ -4,7 +4,7 @@
 /opt/python/cp37-cp37m/bin/pip install cmake
 
 # pyslise
-pythons=(cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310)
+pythons=(cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313)
 for py in ${pythons[@]}; do
     mkdir /opt/matslise-build-${py}
     cd /opt/matslise-build-${py}
@@ -14,5 +14,5 @@ for py in ${pythons[@]}; do
 
     /opt/python/cp37-cp37m/bin/cmake --build . --target build_wheel --config Release -- -j 6
 
-    auditwheel repair matslise/dist/*${py}*.whl --plat manylinux2010_x86_64 -w /opt/matslise/wheelhouse
+    auditwheel repair matslise/dist/*${py}*.whl -w /opt/matslise/wheelhouse
 done
