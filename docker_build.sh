6ee488388e5570f19d6add1819892dc70e1954e9
Toon Baeyens <toon.baeyens@gmail.com>
2025-10-26 22:36:50 +0100

    Test docker_build.sh, bump mac version

--- docker_build.sh
+++ docker_build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+set -e
+
+cmake_python=/opt/python/cp312-cp312
 # cmake
-/opt/python/cp37-cp37m/bin/pip install cmake
+${cmake_python}/bin/pip install cmake
 
 # pyslise
 pythons=(cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313)
 for py in ${pythons[@]}; do
     mkdir /opt/matslise-build-${py}
     cd /opt/matslise-build-${py}
-    /opt/python/cp37-cp37m/bin/cmake /opt/matslise/ \
+    /opt/python/${py}/bin/pip install setuptools
+    ${cmake_python}/bin/cmake /opt/matslise/ \
         -DCMAKE_BUILD_TYPE=Release \
         -DPYTHON_EXECUTABLE=/opt/python/${py}/bin/python
 
-    /opt/python/cp37-cp37m/bin/cmake --build . --target build_wheel --config Release -- -j 6
+    ${cmake_python}/bin/cmake --build . --target build_wheel --config Release -- -j 6
 
     auditwheel repair matslise/dist/*${py}*.whl -w /opt/matslise/wheelhouse
 done
